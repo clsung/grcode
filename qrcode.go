46fb13b08f3410533693a80ddacea8063839f18b
Vinicius Vasconcelos <vasconcelosvcd@gmail.com>
2018-07-19 13:08:04 -0300

    implement function that reads from image.Image golang class

--- qrcode.go
+++ qrcode.go
@@ -40,3 +40,19 @@ func GetDataFromFile(imagePath string) (results []string, err error) {
 	}
 	return results, nil
 }
+
+//GetDataFromImage read qrcode directly from golang Image class
+func GetDataFromImage(image image.Image) (results []string, err error) {
+
+	scanner := NewScanner()
+	defer scanner.Close()
+	scanner.SetConfig(0, C.ZBAR_CFG_ENABLE, 1)
+	zImg := NewZbarImage(image)
+	defer zImg.Close()
+	scanner.Scan(zImg)
+	symbol := zImg.GetSymbol()
+	for ; symbol != nil; symbol = symbol.Next() {
+		results = append(results, symbol.Data())
+	}
+	return results, nil
+}
